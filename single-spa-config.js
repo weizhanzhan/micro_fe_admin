5b8eb509576d91cc09c5e027d2f4e9add9637eae
weizhanzhan <375786117@qq.com>
2020-06-02 16:12:37 +0800

    test

--- single-spa-config.js
+++ single-spa-config.js
@@ -3,17 +3,18 @@
 ;(function() {
   Promise.all([System.import('single-spa')]).then( function (modules) {
     var singleSpa = modules[0]
+    console.log(singleSpa)
+ 
 
     singleSpa.registerApplication(
-      'single-spa-playground',
-      function(){
-        return System.import('single-spa-playground')
+      'base',
+      ()=>{
+        return System.import('base')
       },
-      function(location){
-        return location.pathname.startsWith('/vue')
-      }
+      location=> true
     );
-
+    console.log(2)
     singleSpa.start();
+    console.log(3)
   })
 })()
